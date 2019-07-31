5b5caf50ae3149b87831af6eeaebecf378c4de96
eshort21 <53440516+eshort21@users.noreply.github.com>
2019-07-31 10:09:18 -0400

    Add files via upload

---
+++ code assignment 2.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+def cadbury(c):
+    cadbury = c+10
+    
+    print(cadbury)
+    
+cadbury(5)
+
+def pineapple(a):
+    pineapple = a+10
+    
+    print(pineapple)
+    
+pineapple(3)
+
+cadbury=(5)
+cadburyBox = cadbury +10
+print(cadburyBox)
+
+cadburyBox = 15
+
+def cadburyBox(c):
+    cadburyBox = c+5
+    
+    print(cadburyBox)
+    
+cadburyBox(15)
+
+# different way
+def cadbury(c):
+    cadburyBox = c+5
+    
+    print(cadburyBox)
+    
+cadbury(15)
+
+#chocolate
+def cadburyBox(c,w,d):
+    print("there is",c, ",",d,",",w, "in the box")
+    
+cadburyBox("white chocolate","dark chocolate", "milk chocolate")
+
+#name
+print("enter your name:")
+X = input("emily")
+print("hello" + X)
+
+#age
+print("enter your age:")
+X = input(16)
+print("your age is",+X)
+"""
+#library
+import math
+
+dir(math)
+
+math.sin(60)*5
+"""
+#math library
+def root(v):
+    print("The cubic root of" ,v, "is",math.pow(v,(1/3)))
+    
+root(8)
+"""
+
+#something
+r = int(input("enter a value"))
+
+def value(r):
+    value = math.pow(r,1/3)
+    print("the cubic root of",r, "is",value)
+    
+    
+
+
+    
+                
+
+    
+
+
+    
+
+    
+    
+    
+    
+    
+    
+    
---
+++ math.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 13:19:11 2019
+
+@author: STEM
+"""
+def subtract(a,b):
+    subtract = a-b
+    
+    print(subtract)
+    
+subtract(10,6)
+
+"""
+def multiply(b,h):
+    multiply = (b*h*1/2)
+    
+    print(multiply)
+    
+multiply(10,4)
+    
+def area(b,h):
+    area = (b*h*1/2)
+    
+    print("the area of the triangle with base" ,b, "and height" ,h, "is",area)
+    
+area(10,4)
+
+def add(d,c)
+
