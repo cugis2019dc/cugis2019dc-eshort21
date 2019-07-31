2be0d0c831a19481067ac7906442eace31a804aa
eshort21 <53440516+eshort21@users.noreply.github.com>
2019-07-31 10:08:45 -0400

    Add files via upload

---
+++ coding assignment 1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 11:00:04 2019
+
+@author: STEM
+"""
+
+print("hello my name is emily")
+print("today is july 29")
+print("hello")
+5*2
+5/2      
+5-2
+5**2
+8/9*2
+8/9*3
+print("what")
+print("5*2")
+
+
+def multiply(a,b):
+    multiply = a*b
+    
+    print(multiply)
+    
+multiply(5,6)
